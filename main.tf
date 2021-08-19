4febbfb14c9a6b83cf724e064e07fb9c094f1921
Rakesh <rduggirala@unfi.com>
2021-08-19 19:17:31 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -131,7 +131,9 @@ resource "aws_instance" "hashicat" {
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
-  tags = {Department = "devops"
+  tags = {
+    Department = "devops"
+    Billable = "true"
     Name = "${var.prefix}-hashicat-instance"
   }
 }
